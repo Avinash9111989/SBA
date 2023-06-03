3a7f081d353a9a5d12d3dde4ad6041b1ad501cff
avinash <avinash.talakanti@gmail.com>
2023-06-03 15:43:20 +0530

    hashmap sba update

---
+++ HashMapAssignment2/HashMapAssignment2/src/hashmapassignment2/Tester.java
@@ -0,0 +1,25 @@
+
+//do not modify the above line
+
+package hashmapassignment2;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class Tester {
+	public static Map<String, Integer> mergeMaps(Map<String, Integer> mapOne, Map<String, Integer> mapTwo){
+        //Implement your logic here and change the return statement accordingly
+		return null;
+	}
+
+	public static void main(String args[]) {
+		//create map one and read input key and value for mapone
+		
+		// create maptwo and read input key and value for maptwo
+		Map<String, Integer> mergedMap = mergeMaps(mapOne, mapTwo);
+
+		// print mergedMap
+	
+	}
+}
